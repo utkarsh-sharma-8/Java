42a9eb0974f8e0d3f5dd4d81bf26bda64e171eef
utkarsh-sharma-8 <utkarshsharma3131@gmail.com>
2023-08-31 14:48:13 +0530

    .

---
+++ Missing.java
@@ -0,0 +1,30 @@
+/**
+ * Missing
+ */
+public class Missing {
+
+    public static int missingNumber(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i];
+            if (nums[i] < nums.length && nums[i] != nums[correct]) {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            } else {
+                i++;
+            }
+        }
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index) {
+                return index;
+            }
+        }
+        return nums.length;
+    }
+
+    public static void main(String[] args){
+        int[] nums={3,0,1};
+        System.out.println(missingNumber(nums));
+    }
+}
