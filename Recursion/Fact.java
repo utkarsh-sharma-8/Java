41ef89c4706c589fa5d4673e7f18a8f6cba4c006
utkarsh-sharma-8 <utkarshsharma3131@gmail.com>
2024-02-26 14:34:48 +0530

    .

---
+++ Recursion/Fact.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class Fact {
+    public static  int fact(int n){
+        if(n==1){
+            return n;
+        }
+        return n*fact(n-1);
+    }
+    public static void main(String[] args){
+        System.out.println(fact(5));
+    }
+}
