c87288e37155b95ac7c243d6f6103453cdf96ef8
utkarsh-sharma-8 <utkarshsharma3131@gmail.com>
2024-04-20 15:20:03 +0530

    .

--- trees/Trees1.java
+++ trees/Trees1.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Trees1
  */
@@ -53,7 +56,33 @@ public class Trees1 {
         System.out.print(root.value+" ");
         inorder(root.left);
     }
-    
+    public static void level(Node root){
+        if(root==null){
+            return;
+        }
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while(!q.isEmpty()){
+            Node currNode=q.remove();
+            if(currNode==null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }else{
+                    q.add(null);
+                }
+            }else{
+                System.out.print(currNode.value+" ");
+                if(currNode.left!=null){
+                    q.add(currNode.left);    
+                }
+                if (currNode.right!=null) {
+                    q.add(currNode.right);                    
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree=new BinaryTree();
@@ -61,6 +90,7 @@ public class Trees1 {
         // System.out.println(root.value);
         // preorder(root);
         // inorder(root);
-        postorder(root);
+        // postorder(root);
+        level(root);
     }
 }
