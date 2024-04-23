12d83b2389fd03331a81cf06610c156620420930
utkarsh-sharma-8 <utkarshsharma3131@gmail.com>
2024-04-23 11:08:14 +0530

    .

---
+++ trees/Avl.java
@@ -0,0 +1,8 @@
+public class Avl {
+    private static class Node{
+    int value;
+    Node right;
+    Node left;
+   }
+   public class 
+}
---
+++ trees/BinaryTree.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+public class BinaryTree {
+    private static class Node{
+        int val;
+        Node right;
+        Node left;
+        public Node(int val){
+            this.val=val;
+        }
+    }
+    private Node root;
+
+    public void populate (Scanner scanner){
+        System.out.println("Enter the root Node");
+        int value=scanner.nextInt();
+        root=new Node(value);
+        populate(scanner,root);
+    }
+    private void populate(Scanner scanner, Node node){
+        System.out.println("Do you want to enter left of "+node.val);
+        boolean left =scanner.nextBoolean();
+        if(left){
+            System.out.println("Enter the value of"+node.val);
+            int value=scanner.nextInt();
+            node.left=new Node(value);
+            populate(scanner,node.left);
+        }
+        System.out.println("Do you want to enter right of "+node.val);
+        boolean right =scanner.nextBoolean();
+        if(right){
+            System.out.println("Enter the value of"+node.val);
+            int value=scanner.nextInt();
+            node.right=new Node(value);
+            populate(scanner,node.right);
+        }
+    }
+    public void display(){
+        display(root,"");
+    }
+    private void display(Node node,String indent){
+        if(node==null){
+            return;
+        }
+        System.out.println(indent+node.val);
+        display(node.left,indent+"\t");
+        display(node.right,indent+"\t");
+    }
+    public static void main(String[] args){
+        Scanner scanner=new Scanner (System.in);
+        BinaryTree tree=new BinaryTree();
+        tree.populate(scanner);
+        tree.display();
+    }
+}
