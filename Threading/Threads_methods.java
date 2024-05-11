aba5a40c8610e3deb5c8296c4d5f738a0a543227
utkarsh-sharma-8 <utkarshsharma3131@gmail.com>
2024-05-11 15:40:34 +0530

    .

--- Threading/Threads_methods.java
+++ Threading/Threads_methods.java
@@ -1,20 +1,26 @@
 public class Threads_methods {
     public static void main(String[] args){
         Threads2 obj1=new Threads2();
-        obj1.start();
         B obj2 =new B();
+        C obj3=new C();
+        obj3.setPriority(Thread.MAX_PRIORITY);
+        obj2.setPriority(5);
+        obj1.setPriority(Thread.MIN_PRIORITY);
+        obj1.start();
         obj2.start();
+        obj3.start();
     }
 }
+//sleep using try catch
 class Threads2 extends Thread{
     @Override
     public void run(){
         for(int i=1;i<5;i++){
-                try {
-                    sleep(10000);
-                } catch (Exception e) {
-                    System.out.println(e);
-                }
+                // try {
+                //     sleep(10000);
+                // } catch (Exception e) {
+                //     System.out.println(e);
+                // }
             System.out.println("Thread1 is running");
         }
     }
@@ -25,6 +31,16 @@ class B extends Thread{
         for(int i=1;i<5;i++){
             // if(i==1) yield();
             System.out.println("Thread2 is running");
+        }
+    }
+}
+//set priority 
+class C extends Thread{
+    @Override
+    public void run(){
+        for(int i=1;i<5;i++){
+            // if(i==1) yield();
+            System.out.println("Thread3 is running");
         }
     }
 }
