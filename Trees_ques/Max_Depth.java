7bb82470d6975a5d4283ddc6e0af701052becb85
utkarsh-sharma-8 <utkarshsharma3131@gmail.com>
2024-05-24 11:19:23 +0530

    .

---
+++ Trees_ques/Max_Depth.java
@@ -0,0 +1,78 @@
+public class Max_Depth {
+    public int maxDepth(TreeNode.Node root){
+        if(root==null){
+            return 0;
+        }
+        int left=maxDepth(root.left);
+        int right=maxDepth(root.right);
+        int maxDepth=Math.max(left,right)+1;
+        return maxDepth;
+    }
+}
+// class TreeNode {
+//     public class Node{
+//         public int val;
+//         public Node left;
+//         public Node right;
+//         private int height;
+//         public Node(int val){
+//             this.val=val;
+//         }
+//         public int getValue(){
+//             return val;
+//         }
+//     }
+//     private Node root;
+//     public int height(Node node){
+//         if(node==null){
+//             return -1;
+//         }
+//         return node.height;
+//     }
+//     public boolean isEmpty(){
+//             return root== null;
+//     }
+//     public void insert(int value){
+//         root=insert(value,root);
+//     }
+//     private Node insert(int value,Node node){
+//         if(node==null){
+//             node=new Node(value);
+//             return node;
+//         }
+//         if(value < node.val){
+//             node.left=insert(value,node.left);
+//         }
+//         if(value > node.val){
+//             node.right=insert(value,node.right);
+//         }
+//         node.height=Math.max(height(node.left),height(node.right)) + 1 ;
+//         return node;
+//     }
+//     public void populate(int[] nums){
+//         for(int i=0;i<nums.length;i++){
+//             this.insert(nums[i]);
+//         }
+//     }
+//     public boolean balanced(){
+//         return balanced(root);
+//     }
+//     private boolean balanced(Node node){
+//         if(node==null){
+//             return true;
+//         }
+//         return Math.abs(height(node.left)-height(node.right))<=1 && balanced(node.left) && balanced(node.right);
+//     }
+//     public void display (){
+//         display(this.root,"Root Node: ");
+//     }
+//     private void display(Node node,String details){
+//         if(node==null){
+//             return;
+//         }
+//         System.out.println(details+node.val);
+//         display(node.left,"Left child of"+node.val+" : ");
+//         display(node.right,"Right child of"+node.val+" : ");
+//     }
+
+// }
