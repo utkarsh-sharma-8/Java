05af9fd5b496dc0ee63153dc0f1cf8e5100b62a1
utkarsh-sharma-8 <utkarshsharma3131@gmail.com>
2024-06-20 17:27:50 +0530

    .

---
+++ Primeno.java
@@ -0,0 +1,34 @@
+public class Primeno {
+    public static int countPrimes(int n) {
+        if (n <= 2) {
+           return 0;
+       }
+
+       boolean[] isPrime = new boolean[n];
+       for (int i = 3; i < n; i += 2) {
+           isPrime[i] = true;
+       }
+
+       isPrime[2] = true; 
+
+       for (int i = 3; i * i < n; i += 2) {
+           if (isPrime[i]) {
+               for (int j = i * i; j < n; j += 2 * i) {
+                   isPrime[j] = false;
+               }
+           }
+       }
+
+       int count = 0;
+       for (int i = 2; i < n; i++) {
+           if (isPrime[i]) {
+               count++;
+           }
+       }
+
+       return count;
+   }
+    public static void main(String[] args){
+        System.out.println("count is"+countPrimes(10));
+    }
+}
